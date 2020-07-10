675ae636eb21047f3b1cf4c28aea13d0d3dbbc7d
Michael Stallings <stall84@gmail.com>
2020-07-10 13:30:35 -0400

    Initial commit of basic backend structure with dependencies

---
+++ data/beaches.js
@@ -0,0 +1,534 @@
+
+
+const beachListSE = [
+
+    {
+        
+        name: 'Kill Devil Hills',
+        state: 'NC',
+        lat: 36.034500,
+        lng: -75.668680
+    },
+    {
+        
+        name: 'Wrightsville Beach',
+        state: 'NC',
+        lat: 34.209810,
+        lng: -77.794560
+    },
+    {
+        
+        name: 'Oak Island',
+        state: 'NC',
+        lat: 33.908880,
+        lng: -78.109920
+    },
+    {
+        
+        name: 'Myrtle Beach',
+        state: 'SC',
+        lat: 33.726790,
+        lng: -78.838180
+    },
+    {
+    
+        name: 'Isle Of Palms',
+        state: 'SC',
+        lat: 32.832070,
+        lng: -79.727760
+    },
+    {
+        
+        name: 'Hilton Head Island',
+        state: 'SC',
+        lat: 32.142891,
+        lng: -80.751512
+    },
+    {
+        id: 7,
+        name: 'Tybee Island',
+        state: 'GA',
+        lat: 32.007976,
+        lng: -80.842534
+    },
+    {
+        
+        name: 'Saint Simons',
+        state: 'GA',
+        lat: 31.146156,
+        lng: -81.373035
+    },
+    {
+        
+        name: 'Fernandina Beach',
+        state: 'FL',
+        lat: 30.588244,
+        lng: -81.444224
+    },
+    {
+        
+        name: 'Neptune Beach',
+        state: 'FL',
+        lat: 30.323951,
+        lng: -81.395220
+    },
+    {
+        
+        name: 'Saint Augustine',
+        state: 'FL',
+        lat: 29.844762,
+        lng: -81.265818
+    },
+    {
+        
+        name: 'Daytona Beach',
+        state: 'FL',
+        lat: 29.209943,
+        lng: -81.013252
+    },
+    {
+        
+        name: 'Cape Canaveral',
+        state: 'FL',
+        lat: 28.391568,
+        lng: -80.597803
+    },
+    {
+        
+        name: 'Palm Beach',
+        state: 'FL',
+        lat: 26.706179,
+        lng: -80.033724
+    },
+    {
+    
+        name: 'Fort Lauderdale',
+        state: 'FL',
+        lat: 26.120185,
+        lng: -80.104638
+    },
+    {
+        
+        name: 'Miami Beach',
+        state: 'FL',
+        lat: 25.792868,
+        lng: -80.128592
+    },
+    {
+        
+        name: 'Naples',
+        state: 'FL',
+        lat: 26.253537,
+        lng: -81.822968
+    },
+    {
+        
+        name: 'Clearwater',
+        state: 'FL',
+        lat: 27.979209,
+        lng: -82.825703
+    },
+    {
+        
+        name: 'Alligator Point',
+        state: 'FL',
+        lat: 29.894932,
+        lng: -84.381752
+    },
+    {
+        
+        name: 'Panama City Beach',
+        state: 'FL',
+        lat: 30.208452,
+        lng: -85.863701
+    },
+    {
+        
+        name: 'Pensacola',
+        state: 'FL',
+        lat: 30.336056,
+        lng: -87.144083
+    },
+    {
+        
+        name: 'Gulf Shores',
+        state: 'AL',
+        lat: 30.248322,
+        lng: -87.687664
+    },
+    {
+        
+        name: 'Dauphin Island',
+        state: 'AL',
+        lat: 30.249981,
+        lng: -88.127438
+    },
+    {
+        
+        name: 'Biloxi',
+        state: 'MS',
+        lat: 30.391545,
+        lng: -88.967007
+    },
+    {
+        
+        name: 'Bay Saint Louis',
+        state: 'MS',
+        lat: 30.299605,
+        lng: -89.336567
+    }
+]
+
+const beachListNE = [
+    {
+       
+        name: 'Virginia Beach',
+        state: 'VA',
+        lat: 36.842588,
+        lng: -75.973295
+    },
+    {
+        
+        name: 'Huntington Beach',
+        state: 'VA',
+        lat: 37.018675,
+        lng: -76.453290
+    },
+    {
+        
+        name: 'Ocean City',
+        state: 'MD',
+        lat: 38.367767,
+        lng: -75.073929   
+    },
+    {
+        
+        name: 'Rehoboth Beach',
+        state: 'DE',
+        lat: 38.716700,
+        lng: -75.078282
+    },
+    {
+       
+        name: 'Cape May',
+        state: 'NJ',
+        lat: 38.716700,
+        lng: -75.078282
+    },
+    {
+        
+        name: 'Atlantic City',
+        state: 'NJ',
+        lat: 39.359054,
+        lng: -74.418770
+    },
+    {
+        
+        name: 'Seaside Heights',
+        state: 'NJ',
+        lat: 39.934448,
+        lng: -74.078959
+    },
+    {
+        
+        name: 'Sea Bright Beach',
+        state: 'NJ',
+        lat: 40.361199,
+        lng: -73.973615
+    },
+  
+    {
+        name: 'Ashbury Park',
+        state: 'NJ',
+        lat: 40.223769,
+        lng: -73.998338
+    },
+
+    {
+        
+        name: 'Coney Island',
+        state: 'NY',
+        lat: 40.571335,
+        lng: -73.997345
+    },
+    {
+        
+        name: 'Massapequa', 
+        state: 'NY',
+        lat: 40.651920,
+        lng: -73.484821
+    },
+    {
+       
+        name: 'Islip',
+        state: 'NY',
+        lat: 40.706563,
+        lng: -73.214869
+    },
+    {
+        
+        name: 'Norwalk',
+        state: 'CT',
+        lat: 41.090745,
+        lng: -73.400233
+    },
+    {
+        
+        name: 'West Haven',
+        state: 'CT',
+        lat: 41.258898,
+        lng: -72.941249
+    },
+    {
+        
+        name: 'Old Saybrook',
+        state: 'CT',
+        lat: 41.274018,
+        lng: -72.393675
+    },
+    {
+        
+        name: 'Niantic',
+        state: 'CT',
+        lat: 41.324388,
+        lng: -72.188900
+    }
+    {
+        
+        name: 'Charlestown', 
+        state: 'RI',
+        lat: 41.363149,
+        lng: -71.625638
+    },
+    {
+        
+        name: 'Narragansett',
+        state: 'RI',
+        lat: 41.437332,
+        lng: -71.453316
+    },
+    {
+        
+        name: 'New Bedford',
+        state: 'MA',
+        lat: 41.602041,
+        lng: -70.902126
+    },
+    {
+        
+        name: 'Provincetown',
+        state: 'MA',
+        lat: 42.041523,
+        lng: -70.193751
+    },
+    {
+        
+        name: 'Plymouth', 
+        state: 'MA',
+        lat: 41.952088,
+        lng: -70.653777
+    },
+    {
+      
+        name: 'Manchester by the Sea',
+        state: 'MA',
+        lat: 42.570648,
+        lng: -70763805
+    },
+    {
+        
+        name: 'Rye',
+        state: 'NH', 
+        lat: 43.027888,
+        lng: -70.725579
+    },
+    {
+        
+        name: 'Saco',
+        state: 'ME',
+        lat: 43.484819,
+        lng: -70.385979
+    },
+    {
+        
+        name: 'Camden',
+        state: 'ME',
+        lat: 44.203870,
+        lng: -69.059081
+    },
+]
+
+const beachListS = [
+    {
+        name: 'Franklin',
+        state: 'LA',
+        lat: 29.737246,
+        lng: -91.853467
+    },
+    {
+        name: 'Cameron',
+        state: 'LA',
+        lat: 29.762427,
+        lng: -93.691644
+    },
+    {
+        name: 'Galveston',
+        state: 'TX',
+        lat: 29.270647,
+        lng: -94.821265
+    },
+    {
+        name: 'Freeport',
+        state: 'TX',
+        lat: 28.950847,
+        lng: -95.347805
+    },
+    {
+        name: 'Corpus Christi',
+        state: 'TX',
+        lat: 27.587801,
+        lng: -97.223022
+    },
+    {
+        name: 'South Padre Island',
+        state: 'TX',
+        lat: 26.118304,
+        lng: -97.170382
+    }
+]
+
+const beachListS = [
+    {
+        name: 'Coronado', 
+        state: 'CA',
+        lat: 32.660316,
+        lng: -117.157389
+    },
+    {
+        name: 'Encinitas',
+        state: 'CA', 
+        lat: 33.047263,
+        lng: -117.297616
+    },
+    {
+        name: 'San Clemente',
+        state: 'CA',
+        lat: 33.410935,
+        lng: -117.610365
+    },
+    {
+        name: 'Laguna Beach',
+        state: 'CA',
+        lat: 33.545406,
+        lng: -117.781562
+    },
+    {
+        name: 'Newport Beach',
+        state: 'CA',
+        lat: 33.629010,
+        lng: -117.955328
+    },
+    {
+        name: 'Long Beach',
+        state: 'CA',
+        lat: 33.764614,
+        lng: -118.196681
+    },
+    {
+        name: 'El Segundo',
+        state: 'CA',
+        lat: 33.920847,
+        lng: -118.415241
+    },
+    {
+        name: 'Malibu',
+        state: 'CA',
+        lat: 34.039599,
+        lng: -118.893999
+    },
+    {
+        name: 'Ventura',
+        state: 'CA',
+        lat: 34.282108,
+        lng: -119.293107
+    },
+    {
+        name: 'Santa Barbara',
+        state: 'CA',
+        lat: 34.407710,
+        lng: -119.693902
+    },
+    {
+        name: 'Oceano',
+        state: 'CA',
+        lat: 35.106693,
+        lng: -120.626782
+    },
+    {
+        name: 'Big Sur',
+        state: 'CA',
+        lat: 36.075001,
+        lng: -121.503793
+    },
+    {
+        name: 'Santa Cruz',
+        state: 'CA',
+        lat: 36.951440,
+        lng: -122.026685
+    },
+    {
+        name: 'Half Moon Bay',
+        state: 'CA',
+        lat: 37.466627,
+        lng: -122.444598
+    },
+    {
+        name: 'Pacifica',
+        state: 'CA',
+        lat: 37.635275,
+        lng: -122.490756
+    },
+    {
+        name: 'Muir Beach',
+        state: 'CA',
+        lat: 37.861986,
+        lng: -122.575177
+    },
+    {
+        name: 'Bodega Bay',
+        state: 'CA',
+        lat: 38.341251,
+        lng: -123.049074
+    },
+    {
+        name: 'Jenner',
+        state: 'CA',
+        lat: 38.449571,
+        lng: -123.114997
+    },
+    {
+        name: 'The Sea Ranch',
+        state: 'CA',
+        lat: 38.671701,
+        lng: -123.415634
+    },
+    {
+        name: 'Mendocino',
+        state: 'CA',
+        lat: 39.309413,
+        lng: -123.795893
+    },
+    {
+        name: 'Westport',
+        state: 'CA',
+        lat: 39.652363,
+        lng: -123.783931
+    },
+    {
+        name: 'Samoa',
+        state: 'CA',
+        lat: 40.818802,
+        lng: -124.186522
+    }
+
+]
