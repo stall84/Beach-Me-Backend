9fefd27f6c5bfa5d84da3f05b29201ee3aa6b99d
Michael Stallings <stall84@gmail.com>
2020-07-17 11:59:12 -0400

    changed local env file and added dotenv dep.. wired up get-trips post controller to query distance matrix api and return results

--- controllers/getTrips.js
+++ controllers/getTrips.js
@@ -1,22 +1,40 @@
+const distance = require('google-distance-matrix');
+const Beaches = require('../models/Beaches');
+require('dotenv').config();
+
+
+
 
 // @description: Query Google Distance Matrix for trip times to selected beaches
 // @route GET /api/v1/get-trips
 // @access PUBLIC (no auth)
 
-const Beaches = require('../models/Beaches');
+// Configure Google Distance Matrix API request
+distance.key(process.env.GOOGLE_API_KEY);
+
 
 exports.getTrips = async (req,res,next) => {
     try {
         // format the incoming request body in google's distance matrix format
+        let travelmode = 'DRIVING'
         let origin = [`${req.body.reduxLat},${req.body.reduxLng}`];
         let destinations = [];
         // 'loop' through req body's large searchBeach array and pull out just the nested lat and lng. no spaces per google's formating
         req.body.searchBeaches.forEach(beach => {
-            destinations.push(`${beach.location.coordinates[0].$numberDecimal},${beach.location.coordinates[1].$numberDecimal}`)
+            destinations.push(`${beach.location.coordinates[1].$numberDecimal},${beach.location.coordinates[0].$numberDecimal}`)
+        })
+        distance.matrix(origin, destinations, travelmode, function(error, distances) {
+            if (!error) {
+                res.json(distances.rows);
+                console.log('Google Distance Response: ', distances);
+            }
         })
         
         console.log('Origin: ', origin)
         console.log('Destinations: ', destinations)
+        
+
+
         //console.log('Redux Coords from Front: ', req.body.reduxLat, req.body.reduxLng)
     } catch (error) {
         console.log('Error on getTrips Controller POST request: ', error)
